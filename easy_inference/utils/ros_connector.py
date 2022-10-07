f33dfe782f6d52b87ef79b0337d368c78788a26d
Chadi Salmi <c.salmi@tudelft.nl>
2022-10-07 16:52:24 +0200

    updates to the ros_connector

--- easy_inference/utils/ros_connector.py
+++ easy_inference/utils/ros_connector.py
@@ -1,6 +1,7 @@
 from easy_inference.utils.boundingbox import BoundingBox3d
 import jsk_recognition_msgs.msg as jsk_msgs
-from geometry_msgs.msg import Point, Vector3
+from geometry_msgs.msg import Point, Vector3, PoseStamped
+from std_msgs.msg import Header
 import rospy
 import tf
 from typing import List
@@ -20,25 +21,40 @@ def _it(self):
 Vector3.__iter__ = _it
 
 class RosConnector():
-    def __init__(self, name='person_detection'):
+    def __init__(self, name='person_detection', fixed_frame=None):
         rospy.init_node(name)
         self._tf_listener = tf.TransformListener()
-        # publisher = rospy.Publisher('/detections3D', BoundingBox3DArray, queue_size=1) 
         self._publisherBoxes3d = rospy.Publisher('detections3D', jsk_msgs.BoundingBoxArray, queue_size=1) 
+
+        self._fixed_frame = fixed_frame
 
     def _to_bb_msg(self, box: BoundingBox3d):
         msg = jsk_msgs.BoundingBox()
         msg.pose.position = Point(x=box.x, y=box.y, z=box.z)
         msg.pose.orientation.w = 1
-        msg.dimensions = Vector3(box.w, box.h, box.l)
         msg.header.frame_id = f'camera{int(box.batch_id)+1}_color_optical_frame'
+        msg.dimensions = Vector3(box.w, box.h, box.l)
+
+        if self._fixed_frame is not None:
+            msg.pose = self._tf_listener.transformPose(
+                self._fixed_frame, 
+                PoseStamped(
+                    header=Header(frame_id=f'camera{int(box.batch_id)+1}_color_optical_frame'),
+                    pose=msg.pose
+                )
+            )
+            msg.header.frame_id = self._fixed_frame
+
         return msg
 
     def publishBoundingBoxes3d(self, boxes: List[BoundingBox3d]):
         msg = jsk_msgs.BoundingBoxArray()
         msg.boxes = [self._to_bb_msg(box) for box in boxes]
         msg.header.stamp = rospy.Time.now()
-        msg.header.frame_id = 'velodyne'
+        if self._fixed_frame is not None:
+            msg.header.frame_id = self._fixed_frame
+        else:
+            msg.header.frame_id = 'map'
         self._publisherBoxes3d.publish(msg)
 
         # # NOTE: weird zeros behavior
