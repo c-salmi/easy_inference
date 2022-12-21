5115ac78e503bac3fb930162486c8e9158ce013a
Chadi Salmi <c.salmi@tudelft.nl>
2022-12-21 13:51:11 +0100

    version update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
   long_description = f.read()
 
 setup(name='easy_inference',
-      version='0.0.4',
+      version='0.1.0',
       description='Got a working detection model file? Want to quickly setup inference pipelines? You are in the right place!',
       author='Chadi Salmi',
       license='MIT',
